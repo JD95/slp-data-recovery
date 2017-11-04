c00ba90896d0abc900fc37b2cb44221826ac3376
Jeff <jeffreydwyer95@hotmail.com>
2017-11-03 18:15:47 -0700

    First

---
+++ src/Main.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Prelude ()
+import Protolude
+
+import Data.Semigroup ((<>))
+import Options.Applicative
+import Lens.Micro.Platform
+import qualified Data.ByteString as B
+
+data Options = Options { message :: Text }
+
+opts :: Parser Options
+opts = Options <$> option auto
+     ( long "message"
+    <> short 'm'
+    <> metavar "MESSAGE"
+    <> help "The message to print"
+    <> showDefault
+    <> value "Hello, World!"
+      )
+
+optsInfo :: ParserInfo Options
+optsInfo = info (opts <**> helper)
+  ( fullDesc
+  <> progDesc "Print a simple message"
+  <> header "slp-data-recovery - a minimal application"
+  )
+
+specificFile = "data/070717-C10-F-76-PWS/070717-c10-F-76-PWS-7.wav"
+
+extractData date = snd . B.breakSubstring date 
+
+main :: IO ()
+main = do
+  options <- execParser optsInfo
+  putStrLn (message options)
+
